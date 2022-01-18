25b7f9cf65741320d8f75229c13f5006dfe09984
Jose Sison <68295496+josesison1@users.noreply.github.com>
2022-01-17 19:06:17 -0800

    Update treasurehunt.js

--- treasurehunt.js
+++ treasurehunt.js
@@ -1,52 +1,29 @@
-// alert(`You are live and direct!`)
+// to test if javascript is connected to html below:
+// alert(`You are live and direct!`) <--------------
 
 
-// A function that resets the title of the page to ---> "Michael Jordan Treasure Hunt"
-// code below:
-
+// a function to change the title when clicked below:
 // const basketball = () => {
 //    document.getElementById("title").innerHTML = "Greatest Of All Time"
 // }
 
 
-
-//
-//   var mj = Math.floor(Math.random() * 9)
-//   var bomb = Math.floor(Math.random() * 9)
-//
-// while (mj === bomb) {
-//   var bomb = Math.floor(Math.random() * 9)
-// }
-//
-// const treasure = (n) => {
-//   if (n == mj) {
-//   document.getElementById(id).innerHTML = "&#x1f308"
-//   alert('You are goated!')
-// }  else if (n === bomb) {
-//   document.getElementById(id).innerHTML = "&#x2620"
-//   alert('You lose!')
-// } else {
-//   document.getElementById(id).innerHTML = "&#x1f332"
-// }
-// }
-
-const bomb = Math.floor(Math.random()*9)
-
-const treasure = (id) => {
-  if (id === bomb){
-     document.getElementById(id).innerHTML = "&#x2620"
-    alert("You lose!")
-    // document.getElementById(lose).innerHTML = '<img src="skull.png"/>'
-  } else {
-     document.getElementById(id).innerHTML = "&#10060"
-  }
+if (bomb === mj) {
+  bomb = Math.floor(Math.random() * 9)
 }
-//
 
+var mj = Math.floor(Math.random() * 9)
+var bomb = Math.floor(Math.random() * 9)
 
-
-
-// var id = Math.floor(Math.random() * 12)
-// const rainbow = () => {
-//    return document.getElementById(id).innerHTML = "&#x1f332"
-// }
+const treasure = (c) => {
+  if (c === mj) {
+    document.getElementById(c).innerHTML = '<img src="jordan.jpg"/>'
+    alert("You win!")
+    // setTimeout(() => { window.location.reload(); }, 575); <------- reloads after message pops up
+  } else if (c === bomb) {
+    document.getElementById(c).innerHTML = '<img src="lebron.png"/>'
+    alert("You lose!")
+  } else {
+    document.getElementById(c).innerHTML = "&#10060"
+  }
+ }
